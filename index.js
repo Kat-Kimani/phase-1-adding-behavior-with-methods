4dc8e30b6045fd01ff0e331bc525aba27230d280
catherine <wachukimani@gmail.com>
2023-03-31 04:53:33 +0300

    not passing tests

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+// Your code here
+class Cat {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+cat = new Cat("Sasha", "female");
+cat.speak();
+
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+let dog = new Dog('Rufio', 'male');
+dog.speak();
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name
+        this.sex = sex
+    }
+    speak() {
+        if (this.sex === male) {
+            return `It\'s me! ${this.name} the parrot!`
+
+        } else if (this.sex === female){
+           return  `${this.name} says squawk!`
+        }
+
+    }
+}
